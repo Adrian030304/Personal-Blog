303636d3001c2a97e91ddd33f0f2d51845dbbdfd
Adrian030304 <adrianromanenco1@gmail.com>
2025-09-08 14:45:32 +0300

    sanitize fields

--- utils.py
+++ utils.py
@@ -1,4 +1,4 @@
-import json, os
+import json, os, re
 
 def existing_users_file(file):
     if os.path.exists(file):
@@ -11,3 +11,10 @@ def existing_users_file(file):
 def save_users(users, file):
     with open(file, 'w+') as save:
         json.dump(users, save)
+
+def sanitize_title(title):
+    safe_title = re.sub(r'[^a-zA-Z0-9_-]', '_', title)
+    return safe_title
+def sanitize_date(date):
+    safe_date = re.sub(r'[^0-9]', '-', date)
+    return safe_date
